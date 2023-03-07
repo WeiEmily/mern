4a1ec3d416f4643230670a9236b1c0ccf64b7ac5
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-03-07 21:38:29 +0000

    Initial REST API for goals

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,36 +1,59 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
 
 //@desc Get goals 
 //@route Get /api/goals
 //@access Private
-const getGoals = async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
-}
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find()
+
+    res.status(200).json(goals);
+})
 
 //@desc Set goals 
 //@route POST /api/goals
 //@access Private
-const setGoals = async (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')  
     }
-    res.status(200).json({message: 'Set goal'})
-}
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
+})
 
 //@desc update goals 
 //@route PUT /api/goals/:id
 //@access Private
-const updateGoals = async (req, res) => {
-    res.status(200).json({message: `update goal ${req.params.id}`})
-}
+const updateGoals = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error ('Goal not found ')
+    }
+    const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new : true,
+    })
+    res.status(200).json(updateGoal)
+})
 
 //@desc delete goals 
 //@route delete /api/goals/:id
 //@access Private
-const deleteGoals = async (req, res) => {
-    res.status(200).json({message: `delete goal ${req.params.id}`})
-}
+const deleteGoals = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error ('Goal not found ')
+    }
+    const updateGoal = await Goal.findByIdAndRemove(req.params.id, req.body, {
+        new : true,
+    })
+    
+    res.status(200).json({id: req.params.id})
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv =  require('dotenv').config(); 
 const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db')
 const port = process.env.PORT 
+
+connectDB()
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(errorHandler);
+
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
