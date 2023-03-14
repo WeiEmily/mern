2ddc46a30a13cf19db4817aceb06d0fa94d5504f
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-03-14 21:59:51 +0000

    configure sign in and get info after sign in

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// to start it run :"npm run server"
 const express = require('express');
 const colors = require('colors');
 const dotenv =  require('dotenv').config(); 
@@ -11,9 +12,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(errorHandler);
+
 
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`)); 
